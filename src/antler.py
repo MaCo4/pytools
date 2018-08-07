e1d9c4e194f6bbef43a588a4f4d60d9d3959d315
Magnus Conrad Hyll <magnus@hyll.no>
2018-08-07 15:12:37 +0200

    Antler: Saves a track index file, local tracks support

--- src/antler.py
+++ src/antler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import sys
 from pprint import pprint
@@ -7,9 +8,39 @@ import spotipy
 from spotipy import util as sputil
 
 
+_track_index = None
+def get_track_index():
+    global _track_index
+
+    if _track_index is None:
+        with open('spotify_track_index.json') as file:
+            _track_index = json.load(file)
+
+    return _track_index
+
+
+def save_track_index():
+    global _track_index
+    with open('spotify_track_index.json', 'w') as file:
+        json.dump(_track_index, file)
+
+
+def is_same_track(track1_id, track2_id):
+    if track1_id == track2_id:
+        return True
+    else:
+        track_index = get_track_index()
+        track1 = track_index.get(track1_id, None)
+        track2 = track_index.get(track2_id, None)
+
+        # TODO
+
+
 def backup_all_playlists(sp: spotipy.Spotify, file=None):
     all_playlists = []
+    all_tracks = {}
     playlists_offset = 0
+
     while True:
         playlists = sp.current_user_playlists(limit=50, offset=playlists_offset)
         for playlist in playlists['items']:
@@ -18,17 +49,32 @@ def backup_all_playlists(sp: spotipy.Spotify, file=None):
                 'collaborative': playlist['collaborative'],
                 'public': playlist['public'],
                 'uri': playlist['uri'],
-                'ownerId': playlist['owner']['id'],
+                'owner_id': playlist['owner']['id'],
                 'tracks': []
             }
 
             tracks_offset = 0
             while True:
                 playlist_tracks = sp.user_playlist_tracks(user=current_list['owner_id'], playlist_id=playlist['id'],
-                                                          fields='items.track.uri,next', limit=100,
+                                                          fields='items(is_local,track(uri,id,name,artists.name,'
+                                                                 'album.name)),next',
+                                                          limit=100,
                                                           offset=tracks_offset)
-                for playlist_items in playlist_tracks['items']:
-                    current_list['tracks'].append(playlist_items['track']['uri'])
+                for playlist_item in playlist_tracks['items']:
+                    track = playlist_item['track']
+                    track_id = track['id']
+
+                    # Local tracks doesn't have a Spotify ID, so we'll use its Spotify URI as an ID instead
+                    if playlist_item['is_local']:
+                        track_id = track['uri']
+
+                    current_list['tracks'].append(track_id)
+                    if track_id not in all_tracks:
+                        all_tracks[track_id] = {
+                            'name': track['name'],
+                            'artists': [artist['name'] for artist in track['artists']],
+                            'album': track['album']['name'],
+                        }
 
                 if playlist_tracks['next'] is None:
                     break
@@ -42,13 +88,20 @@ def backup_all_playlists(sp: spotipy.Spotify, file=None):
         playlists_offset += 50
 
     print('Total num lists: {}'.format(len(all_playlists)))
+    print('Total num unique tracks: {}'.format(len(all_tracks)))
+
+    def json_dump_compact(obj, fp):
+        json.dump(obj=obj, fp=fp, separators=(',', ':'))
 
     if file is None:
         user_id = sp.me()['id']
-        with open('spotify_backup_{}.json'.format(user_id), 'w') as file:
-            json.dump(all_playlists, file)
+        with open('spotify_backup_{}_{}.json'.format(user_id, datetime.now().strftime('%Y-%m-%d_%H%M')), 'w') as file:
+            json_dump_compact(all_playlists, file)
     else:
-        json.dump(all_playlists, file)
+        json_dump_compact(all_playlists, file)
+
+    with open('spotify_track_index.json', 'w') as track_index_file:
+        json_dump_compact(all_tracks, track_index_file)
 
 
 if __name__ == "__main__":
--- src/asynceverything.py
+++ src/asynceverything.py
@@ -20,19 +20,40 @@ async def test2():
 
 
 async def test3():
-    await asyncio.sleep(0.3)
+    #await asyncio.sleep(0.3)
     print("Waddlin' here")
     await asyncio.sleep(0.3)
     print("Waddlin' here")
     await asyncio.sleep(0.3)
     print("Waddlin' here")
+
+
+async def test4():
+    print("Test 4 started")
+    await asyncio.sleep(0.5)
+    print("Test 4 finished")
+
+
+async def test5():
+    await asyncio.sleep(0.1)
+    print("Test 5 started")
+    await asyncio.sleep(0.3)
+    print("Test 5 finished")
 
 
 def main():
     asyncio.ensure_future(test1())
     asyncio.ensure_future(test3())
-    asyncio.get_event_loop().run_until_complete()
+    asyncio.get_event_loop().run_forever()
+
+
+def main2():
+    asyncio.gather(test4(), test5())
+    asyncio.get_event_loop().run_forever()
+    #loop = asyncio.get_event_loop()
+    #loop.run_until_complete(test4())
+    #loop.run_until_complete(test5())
 
 
 if __name__ == "__main__":
-    main()
+    main2()
